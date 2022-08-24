1253a9b86be7dd144d1ddab23220c3259690b886
Mahamodul Hasan Moon <coddermoon@gmail.com>
2022-08-24 13:21:25 +0600

    problemn 1 solving done

---
+++ day1.js
@@ -0,0 +1,34 @@
+/**
+ * ========= === practice Problem 1=== =================
+ * 
+ * 
+
+ * 
+ * ============================================
+ */
+
+    // 1) Write an arrow function that will take 3 parameters, will multiply
+    // the parameters and will return the result.
+
+
+    const arrowThreeParams = (num1,num2,num3)=> num1*num2*num3
+        
+    // 2) Write the following sentence in three lines and print the result:
+    // I am a web developer. I love to code. I love to eat biryani.
+
+    const printThreeLine = `
+    I am a Web Developer
+    I Love Code
+    O Love to Eat beriany`
+  
+
+    /*
+     3) Write an arrow function that will take 2 parameters: One parameter
+    will come from you and the other parameter will be a default
+    parameter. Add these two parameters and return the result.
+
+    */
+
+    const deafult = (num1,num2=6)=> num1+num2
+
+    
